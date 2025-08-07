c76024f866a191bb2638f2efe615f7837fbb977f
Andrii Maltsev <almostholyandrew@gmail.com>
2025-08-07 21:32:00 +0300

    added HW with Objects

---
+++ jsHomework/jsObjects/task1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const student = {};
+
+student.firstName = "Andrii";
+student.lastName = "Maltsev";
+student.age = 28;
+student.courses = ["Math", "History", "Programming"];
+
+console.log(student);
